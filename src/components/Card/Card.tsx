a49136b889adab3ee1c3e647ca3ae17fecb98649
Nikolai <nikolai.ivanov3110@gmail.com>
2023-04-09 11:23:53 +0400

    feat: implement request to api

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,24 +1,21 @@
 import React from "react";
-import { ICard } from "../../type";
 import "./Card.css";
+import { IMovie } from "../../type";
 
-interface Card {
-  product: ICard;
+interface ICard {
+  movie: IMovie;
+  onClick: () => void;
 }
 
-const Card = ({ product }: Card) => {
+const getPosterURL = (poster_path: string) => {
+  return `https://www.themoviedb.org/t/p/w220_and_h330_face/${poster_path}`;
+};
+const Card = ({ movie, onClick }: ICard) => {
   return (
-    <div className="post" data-testid="post">
-      <img src={product.imageUrl} alt={product.title} height="300" />
-      <h2>{product.title}</h2>
-      <h3>{product.country}</h3>
-      <div className="post-length">
-        length: <span>{product.length}</span>
-      </div>
-      <div className="post-difficulty">
-        difficulty: <span>{product.difficulty}</span>
-      </div>
-      <p>{product.description}</p>
+    <div className="post" data-testid="post" onClick={onClick}>
+      <img src={getPosterURL(movie.poster_path)} alt={movie.name} width="200" />
+      <h2>{movie.name}</h2>
+      <p>{movie.first_air_date}</p>
     </div>
   );
 };
