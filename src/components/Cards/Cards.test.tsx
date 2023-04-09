31322860f69867e2427375c5b06e78737b48f198
Nikolai <nikolai.ivanov3110@gmail.com>
2023-04-09 11:35:11 +0400

    fix: implemet correct tests

--- src/components/Cards/Cards.test.tsx
+++ src/components/Cards/Cards.test.tsx
@@ -1,9 +1,23 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
-import Cards from "./Cards";
+import { act } from "react-dom/test-utils";
+import Home from "../../pages/Home/Home";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
-describe("renders learn react link", () => {
-  it("render list", () => {
-    render(<Cards />);
-    expect(screen.getAllByTestId("post").length).toEqual(12);
+describe("Cards", () => {
+  it("show cards after render", async () => {
+    await act(async () => {
+      render(
+        <MemoryRouter initialEntries={["/"]}>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </MemoryRouter>
+      );
+    });
+    let someCard = screen.queryByText("Al-Kabir Awy");
+    expect(someCard).toBe(null);
+    someCard = await screen.findByText("Al-Kabir Awy");
+    expect(someCard).toBeInTheDocument();
   });
 });
