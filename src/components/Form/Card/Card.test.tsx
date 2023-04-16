21d90b7a41582a278b46cdd2d4d6bac64fb89a7d
Nikolai <nikolai.ivanov3110@gmail.com>
2023-04-16 12:12:48 +0400

    fix: implement corrrect tests

--- src/components/Form/Card/Card.test.tsx
+++ src/components/Form/Card/Card.test.tsx
@@ -1,32 +1,57 @@
 import { render, screen } from "@testing-library/react";
-import { Card } from "./Card";
-
-const member = {
-  id: 1,
-  name: "Mock Name",
-  surname: "Mock Surname",
-  date: "2023-03-26",
-  gender: "Mock Gender",
-  category: "Mock Category",
-  imageUrl: "https://example.com/mock-image.jpg",
-  rules: true,
-};
+import Card from "./Card";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "../../../store/store";
 
 describe("Card", () => {
-  it("renders member data correctly", () => {
-    render(<Card member={member} />);
-    expect(screen.getByText("Mock Name")).toBeInTheDocument();
-    expect(screen.getByText("Mock Surname")).toBeInTheDocument();
-    expect(screen.getByText("DoB: 2023-03-26")).toBeInTheDocument();
-    expect(screen.getByText("gender:")).toHaveTextContent(
-      "gender: Mock Gender"
+  it("renders Card component with correct data", () => {
+    const member = {
+      id: 1,
+      name: "Name",
+      surname: "Surname",
+      date: "2023-03-26",
+      gender: "Mock Gender",
+      category: "Mock Category",
+      imageUrl: "https://example.com/mock-image.jpg",
+      rules: true,
+    };
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Provider store={store}>
+          <Routes>
+            <Route
+              path="/"
+              element={<Card member={member} onRemove={() => {}} />}
+            />
+          </Routes>
+        </Provider>
+      </MemoryRouter>
     );
-    expect(screen.getByText("category:")).toHaveTextContent(
-      "category: Mock Category"
-    );
-    expect(screen.getByAltText("Mock Name")).toHaveAttribute(
+    expect(screen.getByRole("heading", { name: /Name/ })).toBeInTheDocument();
+    expect(screen.getByAltText(member.name)).toHaveAttribute(
       "src",
-      "https://example.com/mock-image.jpg"
+      member.imageUrl
     );
+    expect(screen.getByTestId("form")).toBeInTheDocument();
   });
 });
+
+// describe("Card", () => {
+//   it("renders member data correctly", () => {
+//     render(<Card member={member} />);
+//     expect(screen.getByText("Mock Name")).toBeInTheDocument();
+//     expect(screen.getByText("Mock Surname")).toBeInTheDocument();
+//     expect(screen.getByText("DoB: 2023-03-26")).toBeInTheDocument();
+//     expect(screen.getByText("gender:")).toHaveTextContent(
+//       "gender: Mock Gender"
+//     );
+//     expect(screen.getByText("category:")).toHaveTextContent(
+//       "category: Mock Category"
+//     );
+//     expect(screen.getByAltText("Mock Name")).toHaveAttribute(
+//       "src",
+//       "https://example.com/mock-image.jpg"
+//     );
+//   });
+// });
--- src/main.test.tsx
+++ src/main.test.tsx
@@ -1,13 +1,10 @@
-// import React from "react";
-// import { render, screen } from "@testing-library/react";
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import store from "./store/store";
 
-// describe("renders learn react link", () => {
-//   test("render App", () => {
+import store from "./store/store";
+import App from "./App";
+
 describe("App component", () => {
   it("should render without errors", () => {
     render(
@@ -17,6 +14,5 @@ describe("App component", () => {
         </BrowserRouter>
       </Provider>
     );
-    // expect(screen.getByText(/About/i)).toBeInTheDocument();
   });
 });
